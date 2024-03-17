1b710e5883d2fa932a6d41e96a4090ace6805ad4
remorae <me@morine.net>
2024-03-16 19:06:54 -0700

    update names

--- keyboards/keebio/iris/keymaps/remorae/colors.h
+++ keyboards/keebio/iris/keymaps/remorae/colors.h
@@ -1,6 +1,6 @@
 /*
-Copyright 2022 Natalie Plagman
-https://github.com/natrastellar
+Copyright 2024 Mori
+https://github.com/remorae
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
